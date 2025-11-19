b8bc9d2a7895b89aa9d5de05344d7345605cd669
freddykray <rosi.kanal2017@yandex.ru>
2025-11-19 21:25:00 +0300

    Initial commit

---
+++ src/main/java/com/example/changesForKCK/ChangesForKck221Application.java
@@ -0,0 +1,13 @@
+package com.example.changesForKCK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangesForKck221Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangesForKck221Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/changesForKCK/ChangesForKck221ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.changesForKCK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangesForKck221ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
